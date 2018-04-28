1f5de228e4d5125c6884f838301b6019c34a18d2
danryw <danryw@CO1041-22.ece.iastate.edu>
2018-04-27 22:30:47 -0500

    Update test

--- hw5/src/edu/iastate/cs228/hw5/Test.java
+++ hw5/src/edu/iastate/cs228/hw5/Test.java
@@ -1,22 +1,10 @@
 package edu.iastate.cs228.hw5;
 
-import java.util.List;
-import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.Random;
-
 public class Test
 {
 
 	public static void main(String[] args)
 	{
-//		Random rng = new Random();
-//		String outputClassName = "fd";
-//		OutputStream output = System.out;
-//		List<String> words = Arrays.asList("abcdefghij");
-//		PerfectHashGenerator p = new PerfectHashGenerator();
-//		p.generate(words, output, outputClassName, rng);
-		
 		
 		Graph g = new Graph(5);
 		
